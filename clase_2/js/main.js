295849afa4761a66610c51a4abf4b258845d775c
Daniel Riverol <dannyriverol@hotmail.com>
2024-01-23 12:13:07 -0300

    Clase 2 Control de flujos

---
+++ clase_2/js/main.js
@@ -0,0 +1,8 @@
+//Variables
+let nombre; //declaración => no números ni caracteres especiales salvo _ o $
+nombre = "Pablo"; //asignación
+let apellido = "López"; //inicialización
+
+const CLAVE = "oruga"; //las constantes siempre se inicializan
+console.log(apellido);
+console.log(nombre);
