40b64378b96c3401c7515d4d10daaeaeaf6cd3eb
Daniel Riverol <dannyriverol@hotmail.com>
2024-02-20 12:22:29 -0300

    Clase 9 DOM

---
+++ clase_9/js/main.js
@@ -0,0 +1,29 @@
+const productos = [
+  { id: 1, nombre: "arroz integral", precio: 89 },
+  { id: 2, nombre: "papa", precio: 90 },
+  { id: 3, nombre: "tomate", precio: 204 },
+  { id: 4, nombre: "morrón", precio: 870 },
+  { id: 5, nombre: "pan", precio: 240 },
+  { id: 6, nombre: "fideo", precio: 124 },
+  { id: 7, nombre: "fideo", precio: 160 },
+  { id: 8, nombre: "arroz", precio: 500 },
+  
+];
+const personas = [
+  "Matias",
+  "Pablo",
+  "Maria",
+  "Tobias",
+  "Marcia",
+  "Kevin",
+  "Karen",
+  "Federico",
+  "Carolina",
+  "Facundo",
+  "Camila",
+  "Stefi",
+  "Augusto",
+  "Jessica",
+  "Carla",
+  "Bautista",
+];
