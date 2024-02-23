ced4d612fddd0f8292c29d930105e4a5257495c8
Daniel Riverol <dannyriverol@hotmail.com>
2024-02-23 20:20:18 -0300

    after dom

--- after_dom/js/main.js
+++ after_dom/js/main.js
@@ -10,7 +10,12 @@ const productos = [];
 const talles = ["xs", "s", "m", "xl", "xxl"];
 
 //cargar opciones en select
-
+for (const talle of talles) {
+  const option = document.createElement("option");
+  option.value = talle;
+  option.innerText = talle;
+  inputTalle.append(option);
+}
 
 //constructor Producto
 class Producto {
@@ -30,9 +35,43 @@ class Producto {
 //Guardar producto
 function guardarProducto(producto) {
   return productos.push(producto);
-}
+}//redundante
 
 //funcion crearHtml
-
+function crearHtml(arr) {
+  //validar que el aaray no este vacio
+  let html;
+  for (const producto of arr) {
+    html = `
+        <div class="col"> 
+            <div class="card">
+              <div class="card-image">
+                <img class="responsive-img" src=${producto.img} />
+              </div>
+            <div class="card-content">
+                <span class="card-title">${producto.nombre}</span>
+                <p>${producto.descripcion}
+                  <p>Size: ${producto.talle}</p>
+                </p>
+                <p>$${producto.precio}</p>
+            </div>
+            <div class="card-action">
+              <input type= "button" class="btn" id=${producto.id} value="Comprar">
+            </div>
+          </div>
+        </div>`;
+  }
+  contenedor.innerHTML = contenedor.innerHTML + html;
+}
 //Evento para guardar
-
+btnGuardar.addEventListener("click", () => {
+  const producto = new Producto(
+    inputNombre.value,
+    inputDescripcion.value,
+    inputPrecio.value,
+    inputTalle.value,
+    inputImg.value
+  );
+  productos.push(producto)
+  crearHtml(productos)
+});
