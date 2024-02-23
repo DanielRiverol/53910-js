42a2d13087dca03498f8d3a4e7381c6b5608a007
Daniel Riverol <dannyriverol@hotmail.com>
2024-02-22 21:03:39 -0300

    Clase 10 Eventos

--- clase_10/js/main.js
+++ clase_10/js/main.js
@@ -8,6 +8,7 @@ const paises = [
   "Perú",
   "Chile",
   "Polonia",
+  "Canada"
 ];
 const servicios = [
   { id: 1, nombre: "limpieza de cutis", precio: 1200, img: "limpieza.jpg" },
@@ -44,3 +45,112 @@ function filtrarServicio(arr, filtro) {
   });
   return filtrado;
 }
+const caja = document.getElementById("caja");
+///console.log(caja[0]);
+//const titSec= document.querySelectorAll('#div')da null
+
+//addEventListener
+caja.addEventListener("click", () => {
+  console.log("Hiciste click");
+});
+//prefijo onNOMBREEVENTO
+
+/*  caja.onclick = ()=>{
+  console.log("Hiciste click desde el prefijo on");
+} */
+
+/* caja.addEventListener("mousedown", ()=>{
+  console.log("Presionaste el boton");
+})
+caja.addEventListener('mouseup',()=>{
+  console.log("Soltaste el boton");
+}) */
+
+/* caja.addEventListener('mouseenter',()=>{
+  //alert('Entraste a la caja')
+  caja.style.backgroundImage= "url(./img/estados.jpg)"
+})
+caja.addEventListener('mouseleave',()=>{
+
+  caja.style.backgroundImage= "url(./img/australia.jpg)"
+
+})
+caja.addEventListener('click',()=>{
+  caja.style.backgroundImage= "url(./img/pum.png)"
+})
+ */
+//scroll
+const popup = document.querySelector(".pop-up"),
+  btnClose = document.querySelector(".btn-close");
+
+/* function mostrarPopup() {
+  if (window.scrollY > 100) {
+    popup.classList.add("visible");
+  }
+}
+window.addEventListener("scroll", mostrarPopup);
+
+btnClose.addEventListener("click", () => {
+  popup.classList.remove("visible");
+  window.removeEventListener("scroll", mostrarPopup);
+}); */
+
+const ingreso = document.querySelectorAll("input"),
+  btnSearch = document.querySelector("#btnSearch"),
+  contenedor = document.querySelector("#contenedor");
+//console.log(ingreso);
+const inputSearch = ingreso[0];
+
+function crearHtml(el) {
+  //llamo al elemento contenedor
+  contenedor.innerHTML = "";
+  //creo una variable con un estructura html
+  let html = `<div class="card">
+                <img src=" ./img/${el.img}" alt="${el.nombre}">
+                <hr>
+                <h3>${el.nombre}</h3>
+                <p>Precio: $${el.precio} </p>
+                  <div class="card-action">
+                    <button id="${el.id}">Comprar</button>
+                  </div>
+              </div>`;
+  //se la agrego al contenedor
+
+  //modificar para que muestre un cartel producto no encontrado
+  contenedor.innerHTML = html;
+}
+
+/* inputSearch.addEventListener("keyup", () => {
+  const encontrado= buscarServicio(servicios,inputSearch.value)
+  crearHtml(encontrado)
+  console.log(encontrado);
+}); */
+
+btnSearch.addEventListener("click", () => {
+  const encontrado = buscarServicio(servicios, inputSearch.value);
+  crearHtml(encontrado);
+});
+
+const nombre = ingreso[1];
+const edad = ingreso[2];
+
+/* nombre.addEventListener('change',()=>{
+  console.log("Cambio");
+})
+edad.addEventListener('change',()=>{
+  console.log("Cambio");
+}) */
+
+const select = document.querySelector("#paises");
+paises.forEach((pais) => {
+  let option = document.createElement("option");
+  option.value = pais;
+  option.innerText = pais;
+  select.append(option);
+});
+
+select.addEventListener('change', ()=>{
+  let option = select.options[select.selectedIndex].value
+  console.log(option);
+  
+})
