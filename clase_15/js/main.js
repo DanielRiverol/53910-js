aff2250693f8f073be35bc1e6308487ed21c054f
Daniel Riverol <dannyriverol@hotmail.com>
2024-03-12 21:03:29 -0300

    Clase 15 Asincronía y promesas

--- clase_15/js/main.js
+++ clase_15/js/main.js
@@ -33,4 +33,97 @@ function logSquare(num) {
   console.log(square(num));
 }
 
-//logSquare(4);
+//logSquare(2);
+//setTimeOut 1000ms= 1s
+// setTimeout(() => {
+//   console.log("Mientras tanto...");
+// }, 0);
+// console.log("Inicio del proceso");
+// console.log("Fin del proceso");
+
+// btn.addEventListener("click", () => {
+//   setTimeout(() => {
+//     card.classList.remove("active");
+//   }, 2500);
+//   card.classList.add("active");
+// });
+btn.addEventListener("click", () => {
+  btn.innerHTML = `Espere <i class="fa-solid fa-spinner animation"></i>`;
+  card.classList.add("active");
+  btn.disabled = true;
+  setTimeout(() => {
+    btn.innerHTML = "Click";
+    card.classList.remove("active");
+    btn.disabled = false;
+  }, 2500);
+});
+
+// setTimeout(() => {
+//   for (const letra of "Hola") {
+//     console.log(letra);
+//   }
+// }, 2500);
+// setTimeout(() => {
+//   for (const letra of "Juan") {
+//     console.log(letra);
+//   }
+// }, 1500);
+//setInterval
+// let contador = 0;
+// let interval = setInterval(() => {
+//   contador++;
+//   console.log(contador);
+//   if (contador === 10) {
+//     clearInterval(interval);
+//   }
+// }, 1000);
+
+//PROMESAS
+// console.log(
+//   new Promise((resolve, reject) => {
+//     //cuerpo de la promesa
+//   })
+// );
+
+const eventoFuturo = (response) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (response === true) {
+        resolve("Promesa resuelta");
+      } else {
+        reject("Promesa rechazada");
+      }
+    }, 3000);
+  });
+};
+
+//console.log(eventoFuturo(true));
+//console.log(eventoFuturo(false));
+const devolverCD = (response) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (response === true) {
+        resolve("Si tomá gracias por prestarlo");
+      } else {
+        reject("Uh lo perdí");
+      }
+    }, 3000);
+  });
+};
+
+//console.log(devolverCD(false));
+// devolverCD(true).then((res) => {
+//   console.log(res);
+// });
+// devolverCD().catch((error) => {
+//   console.log(error);
+// });
+//console.log("Ejecucion");
+console.log("Te acordas el cd que te presté?");
+devolverCD(false)
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
