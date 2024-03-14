fb49f487e79dfed19745d43a3b424f884140d988
Daniel Riverol <dannyriverol@hotmail.com>
2024-03-14 15:11:11 -0300

    Clase 16 - Ajax y fetch

---
+++ clase_16/js/main.js
@@ -0,0 +1,57 @@
+const card = document.querySelector(".card");
+const lista = document.querySelector(".lista");
+const btn = document.querySelector("#btn"),
+  contenedor = document.querySelector(".contenedor");
+
+const servicios = [
+  { id: 1, nombre: "limpieza de cutis", precio: 1200, img: "limpieza.jpg" },
+  { id: 2, nombre: "masajes", precio: 1800, img: "masajes.jpg" },
+  { id: 3, nombre: "electrodos", precio: 2800, img: "electrodos.jpg" },
+  { id: 4, nombre: "manicura", precio: 1500, img: "manicuria.webp" },
+  { id: 5, nombre: "uñas", precio: 1200, img: "esculpidas.webp" },
+  { id: 6, nombre: "mascarillas", precio: 1900, img: "mascarilla.jpeg" },
+  { id: 7, nombre: "microblanding", precio: 5800, img: "microblanding.png" },
+  {
+    id: 8,
+    nombre: "radiofrecuencia",
+    precio: 12800,
+    img: "radiofrecuencia.png",
+  },
+  {
+    id: 9,
+    nombre: "micropigmentación",
+    precio: 7800,
+    img: "micropigmentacion.jpeg",
+  },
+];
+
+/* Funciones */
+// función que genera la vista de los servicios
+const renderServicios = (arr) => {
+  contenedor.innerHTML = "";
+  let html;
+  for (const item of arr) {
+    const { id, nombre, img, precio } = item;
+    html = `
+        <div class="card">
+      		<div class="card-image">
+        	<img src="../img/${img}">
+        	<span class="card-title">${nombre.toUpperCase()}</span>
+       </div>
+     	 <div class="card-content"> 
+     		<p>$${precio}</p>
+      </div>
+      <div class="card-action">
+        <button class="btn btn-normal" id="${id}">Comprar</button>
+      </div>
+     </div>
+     `;
+    contenedor.innerHTML += html;
+  }
+};
+
+const pedirServicios = (arr) => {
+  //Instanciar promesa
+};
+let products = [];
+
